86972ef6fbcffd320b2800d05a3156ad5ccd9d36
unknown <RockstarJay>
2021-06-04 18:29:19 +0530

    Looping through the simulator stored the singlet in dictonarythen calulated the percentage of occurance of siglet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 echo "Flip Coin Combination Problem"
 
+read -p "How Many Times You Want to Flip The Coin: " n
+declare -A flipResult
+flipResult=( ['H']=0 ['T']=0 )
 isHead=1
-coinToss=$((RANDOM%2))
-if [ $coinToss -eq $isHead ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+for(( i=1; i<=$n; i++ ))
+do
+	flipCoin=$((RANDOM%2))
+	if [ $flipCoin -eq $isHead ]
+	then
+		flipResult['H']=$(("${flipResult['H']}"+1))
+	else
+		flipResult['T']=$(("${flipResult['T']}"+1))
+	fi
+done
+
+headsPercentage=$(((${flipResult['H']}*100)/$n))
+tailsPercentage=$((100-$headsPercentage))
+
+#echo Heads=${flipResult['H']} Tails=${flipResult['T']}
+echo Heads Percentage="$headsPercentage"% Tails Percentage="$tailsPercentage"%
