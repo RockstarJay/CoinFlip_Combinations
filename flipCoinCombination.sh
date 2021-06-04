37d4e62e5d311213b2d68df6ce730537b5114f2d
unknown <RockstarJay>
2021-06-04 18:49:35 +0530

    Loop through th Doublet combination. Got the Combinations as well as percentage of occurance of them.

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,21 +3,38 @@ echo "Flip Coin Combination Problem"
 
 read -p "How Many Times You Want to Flip The Coin: " n
 declare -A flipResult
-flipResult=( ['H']=0 ['T']=0 )
-isHead=1
-for(( i=1; i<=$n; i++ ))
+flipResult=( ['HH']=0 ['HT']=0 ['TH']=0 ['TT']=0 )
+hh=0
+ht=1
+th=2
+tt=3
+for (( i=1; i<=$n; i++ ))
 do
-	flipCoin=$((RANDOM%2))
-	if [ $flipCoin -eq $isHead ]
-	then
-		flipResult['H']=$(("${flipResult['H']}"+1))
-	else
-		flipResult['T']=$(("${flipResult['T']}"+1))
-	fi
+	flipCoin=$((RANDOM%4))
+	case $flipCoin in
+	$hh)
+		flipResult['HH']=$((${flipResult['HH']}+1))
+		;;
+	$ht)
+		flipResult['HT']=$((${flipResult['HT']}+1))
+		;;
+	$th)
+		flipResult['TH']=$((${flipResult['TH']}+1))
+		;;
+	*)
+		flipResult['TT']=$((${flipResult['TT']}+1))
+		;;
+	esac
 done
 
-headsPercentage=$(((${flipResult['H']}*100)/$n))
+hhWinPercentage=$(((${flipResult['HH']}*100)/n))
+htWinPercentage=$(((${flipResult['HT']}*100)/n))
+thWinPercentage=$(((${flipResult['TH']}*100)/n))
+ttWinPercentage=$((100-(hhWinPercentage+htWinPercentage+thWinPercentage)))
+
+headsPercentage=$(($hhWinPercentage+($thWinPercentage+$htWinPercentage)/2))
 tailsPercentage=$((100-$headsPercentage))
 
-#echo Heads=${flipResult['H']} Tails=${flipResult['T']}
-echo Heads Percentage="$headsPercentage"% Tails Percentage="$tailsPercentage"%
+#echo HH=${flipResult['HH']} HT=${flipResult['HT']} TH=${flipResult['TH']} TT=${flipResult['TT']}
+echo "Doublet Heads Percentage=$headsPercentage%"
+echo "Doublet Tails Percentage=$((100-$headsPercentage))%"
