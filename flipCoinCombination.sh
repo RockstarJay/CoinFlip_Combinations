ada4c0c28c8b868dce1b57f57b1b63fe829bcca5
unknown <RockstarJay>
2021-06-04 19:03:57 +0530

    Loop through the triplet combination by creating them with case statement. Then stored these combination in dictionary and then calculated the percentage of the occurance of them

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,38 +3,61 @@ echo "Flip Coin Combination Problem"
 
 read -p "How Many Times You Want to Flip The Coin: " n
 declare -A flipResult
-flipResult=( ['HH']=0 ['HT']=0 ['TH']=0 ['TT']=0 )
-hh=0
-ht=1
-th=2
-tt=3
-for (( i=1; i<=$n; i++ ))
+
+flipResult=( ['HHH']=0 ['HHT']=0 ['HTH']=0 ['HTT']=0 ['THH']=0 ['THT']=0 ['TTH']=0 ['TTT']=0 )
+hhh=0
+hht=1
+hth=2
+thh=3
+htt=4
+tht=5
+tth=6
+ttt=7
+
+for(( i=1; i<=$n; i++ ))
 do
-	flipCoin=$((RANDOM%4))
+	flipCoin=$((RANDOM%8))
 	case $flipCoin in
-	$hh)
-		flipResult['HH']=$((${flipResult['HH']}+1))
+	$hhh)
+		flipResult['HHH']=$((${flipResult['HHH']}+1))
 		;;
-	$ht)
-		flipResult['HT']=$((${flipResult['HT']}+1))
+	$hht)
+		flipResult['HHT']=$((${flipResult['HHT']}+1))
 		;;
-	$th)
-		flipResult['TH']=$((${flipResult['TH']}+1))
+	$hth)
+		flipResult['HTH']=$((${flipResult['HTH']}+1))
+		;;
+	$htt)
+		flipResult['HTT']=$((${flipResult['HTT']}+1))
+		;;
+	$thh)
+		flipResult['THH']=$((${flipResult['THH']}+1))
+		;;
+	$tht)
+		flipResult['THT']=$((${flipResult['THT']}+1))
+		;;
+	$tth)
+		flipResult['TTH']=$((${flipResult['TTH']}+1))
 		;;
 	*)
-		flipResult['TT']=$((${flipResult['TT']}+1))
+		flipResult['TTT']=$((${flipResult['TTT']}+1))
 		;;
 	esac
 done
 
-hhWinPercentage=$(((${flipResult['HH']}*100)/n))
-htWinPercentage=$(((${flipResult['HT']}*100)/n))
-thWinPercentage=$(((${flipResult['TH']}*100)/n))
-ttWinPercentage=$((100-(hhWinPercentage+htWinPercentage+thWinPercentage)))
+hhhWinPercentage=$(((${flipResult['HHH']}*100)/n))
+hhtWinPercentage=$(((${flipResult['HHT']}*100)/n))
+hthWinPercentage=$(((${flipResult['HTH']}*100)/n))
+httWinPercentage=$(((${flipResult['HTT']}*100)/n))
+thhWinPercentage=$(((${flipResult['THH']}*100)/n))
+thtWinPercentage=$(((${flipResult['THT']}*100)/n))
+tthWinPercentage=$(((${flipResult['TTH']}*100)/n))
+tttWinPercentage=$((100-(hhhWinPercentage+hhTWinPercentage+hthWinPercentage+httWinPercentage+thhWinPercentage+thtWinPercentage+tthWinPercentage)))
 
-headsPercentage=$(($hhWinPercentage+($thWinPercentage+$htWinPercentage)/2))
+headsPercentage=$((hhhWinPercentage+((hhtWinPercentage+hthWinPercentage+thhWinPercentage)*2/3)+((httWinPercentage+thtWinPercentage+tthWinPercentage)/3)))
 tailsPercentage=$((100-$headsPercentage))
 
-#echo HH=${flipResult['HH']} HT=${flipResult['HT']} TH=${flipResult['TH']} TT=${flipResult['TT']}
-echo "Doublet Heads Percentage=$headsPercentage%"
-echo "Doublet Tails Percentage=$((100-$headsPercentage))%"
+#echo "HHH=${flipResult['HHH']} HHT=${flipResult['HHT']} HTH=${flipResult['HTH']} HTT=${flipResult['HTT']} THH=${flipResult['THH']} THT=${flipResult['THT']} TTH=${flipResult['TTH']} TTT=${flipResult['TTT']}"
+
+echo "Triplet Heads Percentage: $headsPercentage%"
+echo "Triplet Tails Percentage: $tailsPercentage%"
